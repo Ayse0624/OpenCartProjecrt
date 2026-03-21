64baece579dac5b40273487db2286d1ca7a68906
Ayse0624 <ayixa0624@gmail.com>
2026-03-21 20:46:33 +0300

    son commit ve push

--- src/test/java/pageObjects/deneme.java
+++ src/test/java/pageObjects/deneme.java
@@ -3,6 +3,7 @@ package pageObjects;
 public class deneme {
     public static void main(String[] args) {
         System.out.println("push and pull ogerneiyorum");
+        System.out.println("updated umarim duzelmistir");
     }
     //deneme 1233
 }
