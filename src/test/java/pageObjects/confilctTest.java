d13d4088e931e72b1387116605159918a6f84bec
Ayse0624 <ayixa0624@gmail.com>
2026-03-22 17:09:12 +0300

    conflictTestnnn

--- src/test/java/pageObjects/confilctTest.java
+++ src/test/java/pageObjects/confilctTest.java
@@ -2,6 +2,6 @@ package pageObjects;
 
 public class confilctTest {
     public static void main(String[] args) {
-        System.out.println("bu bir test");
+        System.out.println("xxxxxxxx");
     }
 }
