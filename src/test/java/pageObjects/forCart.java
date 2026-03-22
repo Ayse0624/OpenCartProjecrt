c0999aa5aaa0670671c9e77e7fd1957d79077773
Ayse0624 <ayixa0624@gmail.com>
2026-03-22 16:44:58 +0300

    matematik

--- src/test/java/pageObjects/forCart.java
+++ src/test/java/pageObjects/forCart.java
@@ -4,7 +4,8 @@ public class forCart {
     public static void main(String[] args) {
         System.out.println(";+login brachinden ekledim ");
         System.out.println("cok zor bu isler ");
-         if (1>0){
+        int a=20;
+         if (a>0){
              System.out.println("islem dogru ");
          }
     }
