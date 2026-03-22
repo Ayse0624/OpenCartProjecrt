e9ab6c3834ea2f1d32a7cba6833cfffa715b449e
Ayse0624 <ayixa0624@gmail.com>
2026-03-22 16:36:54 +0300

    bilmem kacinci commitim

--- src/test/java/pageObjects/forCart.java
+++ src/test/java/pageObjects/forCart.java
@@ -3,5 +3,6 @@ package pageObjects;
 public class forCart {
     public static void main(String[] args) {
         System.out.println(";+login brachinden ekledim ");
+        System.out.println("cok zor bu isler ");
     }
 }
