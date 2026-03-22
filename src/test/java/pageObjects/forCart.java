945ee85cc1d9a237bb2e29ea6a95c6aa98608e63
Ayse0624 <ayixa0624@gmail.com>
2026-03-22 16:38:36 +0300

    deneme

--- src/test/java/pageObjects/forCart.java
+++ src/test/java/pageObjects/forCart.java
@@ -4,5 +4,6 @@ public class forCart {
     public static void main(String[] args) {
         System.out.println(";+login brachinden ekledim ");
         System.out.println("cok zor bu isler ");
+        System.out.println("denemee");
     }
 }
